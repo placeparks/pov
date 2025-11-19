5e00e37536ab8a634ccc8269db0954bfa6b7e499
placeparks <86215469+placeparks@users.noreply.github.com>
2025-11-19 14:34:02 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -14,9 +14,9 @@ export function middleware(request: NextRequest) {
     "frame-ancestors 'self' https://*.farcaster.xyz https://*.warpcast.com https://*.farcaster.org https://farcaster.xyz https://warpcast.com",
     "default-src 'self'",
     "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-    "style-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "img-src 'self' data: https:",
-    "font-src 'self' data:",
+    "font-src 'self' data: https://fonts.gstatic.com",
     "connect-src 'self' https: wss:",
   ].join('; ');
 
