cdb2ea583ed5a618c7a637344694009693402287
placeparks <86215469+placeparks@users.noreply.github.com>
2025-11-19 14:17:50 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.farcaster.xyz https://*.warpcast.com https://*.farcaster.org",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
